3735df1d89c98332b830e85a6288e605c51bb977
syedkaiser <syedkaiser@recosenselabs.com>
2022-10-27 13:10:07 +0530

    updated main.py

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-from multiprocessing.sharedctypes import Value
-from urllib import response
 from fastapi import FastAPI
 app = FastAPI()
 
@@ -115,7 +113,7 @@ def add_conversation(meeting_id, meeting_transcribed_text, meeting_translation_t
     
     #inserting encrypted data into mongoDB
     if response:
-        encrypted_data = {"date_time":date_time}
+        encrypted_data = {"meeting_id":meeting_id,"date_time":date_time}
         encrypted_data["meeting_transcribed_text"] = encrypted_meeting_transcribed_text.decode()
         encrypted_data["meeting_translation_text"] = encrypted_meeting_translation_text.decode()
         mycol2.insert_one(encrypted_data)
@@ -132,33 +130,69 @@ def add_conversation(meeting_id, meeting_transcribed_text, meeting_translation_t
     return JSONResponse(response)
 
 
-
-
-
-
-@app.get("/api/get_data")
-def get_data():
-    data = list(mydb.get_collection("unique_ids").find({}))
-    response = []
+@app.get("/api/get_meeting_conversation")
+def get_meeting_conversation(meeting_id):
+    meeting_info = mydb.get_collection("meeting_info").find({"meeting_id":meeting_id})
+    conversations = mydb.get_collection("meeting_conversation").find({})
     
+    response = []
+    decrypt = {}
+    # import pdb;pdb.set_trace()
+    flag = False
     with open("dec_keys.json", 'r') as f:
         dec_keys = json.load(f)        
-        for db_each in data:
+        for db_each in conversations:
             if db_each:
                 for temp in db_each.items():
-                    if "_id" in temp:
-                        continue
-                    else:
-                        text,value = temp
+                    if "meeting_transcribed_text" in temp:
+                        decrypt["meeting_transcribed_text"] = temp[1]
+                    elif "meeting_translation_text" in temp:
+                        decrypt["meeting_translation_text"] = temp[1]
+                        
             for each in dec_keys["decryption_keys"]:
-                if text in each.keys():
-                    dec_key = each.get(text)             
-                    if data:
-                        enc_value = bytes(value, encoding='utf-8')
-                        decrypted_data = json.loads(encryptor.file_decrypt(dec_key, enc_value))
-                        response.append(decrypted_data)
-                        break
-                else:
-                    continue
+                # if value in each.values():
+                flag = True
+                dec_key = each.get("dec_key")             
+                if each:
+                    encoded_meeting_transcribed_text = bytes(decrypt["meeting_transcribed_text"], encoding='utf-8')
+                    encoded_meeting_translation_text = bytes(decrypt["meeting_translation_text"], encoding='utf-8')
+                    decrypted_meeting_transcribed_text = json.loads(encryptor.file_decrypt(dec_key, encoded_meeting_transcribed_text))
+                    decrypted_meeting_translation_text = json.loads(encryptor.file_decrypt(dec_key, encoded_meeting_translation_text))
+                    break
+            else:
+                continue
+    
+    
+    for each in meeting_info:
+        if each:
+            response.append({
+                        "meeting_id":each.get("meeting_id"),
+                        "created_At": each.get("created_At"),
+                        "meeting_date_time":each.get("meeting_date_time"),
+                        "host_name":each.get("host_name"),
+                        "meeting_name": each.get("meeting_name"),
+                        "meeting_source": each.get("meeting_souurce")
+                        })
 
+    temp = {"meeting_transcribed_text": decrypted_meeting_transcribed_text, "meeting_translation_text": decrypted_meeting_translation_text}
+
+    for each in response:
+        each.update(temp)
+        
+    if flag == False:
+        response = {"message":"There is no meeting with the given meeting Id."}
     return response
+
+
+
+@app.delete("/api/delete_meeting")
+def delete_meeting(meeting_id):
+    meeting_info = list(mydb.get_collection("meeting_info").find({"meeting_id":meeting_id}))
+    flag = False
+    for each in meeting_info:
+        if each:
+            mycol1.delete_one(each)
+            flag = True
+    if flag == False:
+        return {"message":"No document present with meeting id: "+meeting_id}        
+    return {"message":"document with meeting id "+meeting_id+" deleted sucessfully"}
